b3ee158373499fb32e53ea3836a789ae9562fd1e
Morokot Sakal <37432981+MorokotSakal@users.noreply.github.com>
2022-04-18 21:14:00 +0700

    Create d_queueManagement_b.ino

---
+++ d_queueManagement_b/d_queueManagement_b.ino
@@ -0,0 +1,65 @@
+#include <Arduino_FreeRTOS.h>
+#include <queue.h>
+QueueHandle_t xQueue;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  xQueue = xQueueCreate(5, sizeof(long));
+
+  if (xQueue != NULL) {
+
+    xTaskCreate( vSenderTask,
+                 "Sender1",
+                 240,
+                 (void *) 100,
+                 1,
+                 NULL);
+    xTaskCreate( vSenderTask,
+                 "Sender2",
+                 240,
+                 (void *) 200,
+                 1,
+                 NULL);
+    xTaskCreate( vReceiverTask,
+                 "Receiver",
+                 240,
+                 (void *) 100,
+                 2,
+                 NULL);
+    vTaskStartScheduler();
+  }
+  else {
+    /* The quee could not be created*/
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void vSenderTask (void *pvParameters) {
+  long lValueToSend;
+  lValueToSend = (long) pvParameters;
+
+  for (;;) {
+    xQueueSend( xQueue, &lValueToSend, portMAX_DELAY );
+    Serial.print( "Sent = ");
+    Serial.println(lValueToSend);
+    taskYIELD();
+  }
+}
+void vReceiverTask( void *pvParameters )
+{
+  long lReceivedValue;
+  for ( ;; ) {
+    if (xQueueReceive( xQueue, &lReceivedValue, portMAX_DELAY ) == pdPASS) {
+      Serial.print( "Received = ");
+      Serial.println(lReceivedValue);
+    }
+    taskYIELD();
+  }
+}
